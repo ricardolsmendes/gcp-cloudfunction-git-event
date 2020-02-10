4eaa7db96577c722d191487068bfe653ddc4e3f9
Ricardo Mendes <ricardolsmendes@gmail.com>
2020-02-09 22:01:14 -0300

    Several improvents to the initial working version

--- index.js
+++ index.js
@@ -4,49 +4,8 @@ const jp = require('jsonpath');
 
 const { CloudBuildClient } = require('@google-cloud/cloudbuild');
 
-const build = Object.create();
-build.steps = [{
-  name: 'gcr.io/cloud-builders/gsutil',
-  args: [
-    'cp',
-    `${process.env.GIT_CREDENTIALS_BUCKET}/*`,
-    '.'
-  ]
-}, {
-  name: 'gcr.io/cloud-builders/gcloud',
-  args: [
-    'kms',
-    'decrypt',
-    '--ciphertext-file', '.git-credentials.enc',
-    '--plaintext-file', '.git-credentials',
-    '--location', process.env.KMS_DECRYPT_LOCATION,
-    '--key', process.env.KMS_KEY,
-    '--keyring', process.env.KMS_KEY_RING
-  ]
-}, {
-  name: 'gcr.io/cloud-builders/git',
-  args: [
-    'config',
-    '--global', 'credential.helper', '\'store\''
-  ]
-}, {
-  name: 'gcr.io/cloud-builders/git',
-  args: [
-    'clone',
-    '--depth', '1'
-  ]
-}, {
-  name: 'gcr.io/cloud-builders/gcloud',
-  args: [
-    'builds',
-    'submit',
-    '--config', process.env.CLOUD_BUILD_CONFIG_FILE,
-    '.'
-  ]
-}];
-
 /**
- * Background Cloud Function to be triggered by an HTTP request.
+ * HTTP Cloud Function to be called on Git repository event.
  *
  * @param {object} req The HTTP request.
  * @param {object} res The HTTP response.
@@ -56,14 +15,27 @@ exports.triggerBuild = async (req, res) => {
   console.log('>> Starting to handle request:');
   console.log(req);
 
-  // TODO: Validate Secret Token
+  // Authorize the request through a specific header.
+  const authHeaderName = process.env.AUTH_HEADER_NAME;
+  const expectedAuthHeaderValue = process.env.AUTH_HEADER_VALUE;
+  const providedAuthHeaderValue = req.get(authHeaderName);
+  if (providedAuthHeaderValue !== expectedAuthHeaderValue) {
+    const errorMessage = [
+      'Unauthorized request!',
+      `Invalid ${process.env.AUTH_HEADER_KEY} header value: ${providedAuthHeaderValue}.`
+    ].join(' ');
+    console.log(`ERROR: ${errorMessage}`);
+    res.status(401).send(errorMessage);
+    return;
+  }
 
-  const repositoryUrl = process.env.GIT_REPOSITORY_URL ?
-    process.env.GIT_REPOSITORY_URL :
-    jp.value(req, process.env.GIT_REPOSITORY_URL_REQUEST_PATH);
+  // Extract repository url from the request body or environment variable.
+  const repositoryUrlRequestPath = process.env.GIT_REPOSITORY_URL_REQUEST_PATH;
+  const repositoryUrl = repositoryUrlRequestPath ?
+    jp.value(req.body, repositoryUrlRequestPath) : process.env.GIT_REPOSITORY_URL;
 
-  // Append the repository url to the `git clone` command.
-  build.steps[3].args.push(repositoryUrl);
+  // Make the Cloud Build specificagion object.
+  const build = makeBuildSpecification(repositoryUrl);
 
   const createBuildRequest = {
     build: build
@@ -79,3 +51,43 @@ exports.triggerBuild = async (req, res) => {
 
   res.status(200).send('Build successfully triggered!');
 };
+
+function makeBuildSpecification(repositoryUrl) {
+
+  const buildSpec = Object.create(null);
+
+  buildSpec.steps = [{
+    name: 'gcr.io/cloud-builders/gcloud',
+    args: [
+      'beta',
+      'secrets',
+      'versions', 'access', process.env.SECRET_VERSION,
+      '--secret', process.env.SECRET_NAME,
+      '>', '.git-credentials'
+    ]
+  }, {
+    name: 'gcr.io/cloud-builders/git',
+    args: [
+      'config',
+      '--global', 'credential.helper', '\'store\''
+    ]
+  }, {
+    name: 'gcr.io/cloud-builders/git',
+    args: [
+      'clone',
+      '--depth', '1',
+      repositoryUrl
+    ]
+  }, {
+    name: 'gcr.io/cloud-builders/gcloud',
+    args: [
+      'builds',
+      'submit',
+      '.',
+      '--config', process.env.CLOUD_BUILD_CONFIG_FILE || 'cloudbuild.yaml',
+      '--substitutions', process.env.CLOUD_BUILD_SUBSTITUTIONS
+    ]
+  }];
+
+  return buildSpec;
+}
