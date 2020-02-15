3d66f54ad4c886de869ace635a229f664b529e75
Ricardo Mendes <ricardolsmendes@gmail.com>
2020-02-15 18:29:31 -0300

    Remove the no longer needed gcloud update command

--- index.js
+++ index.js
@@ -56,7 +56,6 @@ function makeBuildSpecification(repositoryUrl) {
     args: [
       '-c',
       [
-        'gcloud components update &&', // TODO: remove after gcr.io/cloud-builders/gcloud update...
         'gcloud beta', // TODO: remove `beta` after the service turns GA...
         'secrets',
         `versions access ${process.env.SECRET_VERSION}`,
